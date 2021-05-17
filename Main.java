7042762d48d43b5dcd35442da34dbe86b4adf305
AxelLeo129 <axelleo129dev@gmail.com>
2021-05-16 23:33:21 -0600

    JCF

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String args[]) {
         Vista vista = new Vista();
-        vista.execute();
+        vista.start();
     }
 }
--- Vista.java
+++ Vista.java
@@ -1,27 +1,56 @@
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.PriorityQueue;
 
 public class Vista {
-    
-    public Vista() { }
 
-    public void execute() {
+    public Vista() {
+    }
+
+    public void start() {
         Lector lector = new Lector();
         ArrayList<Paciente> pacientes = new ArrayList<>();
         pacientes = lector.readFile("assets/pacientes.txt");
-        VectorHeap<Paciente> pacientes_ordenados = new VectorHeap<>();
+        boolean validacion_opcion = false;
+        Scanner sc = new Scanner(System.in);
+        int version = 0;
+
+        System.out.println("¿Qué versión de árbol quieres usar?");
+
+        while (!validacion_opcion) {
+            System.out.println("1. Vector Heap\n2. Priority Queue (JCF):");
+            try {
+                version = Integer.parseInt(sc.next());
+                if (version == 1 || version == 2)
+                    validacion_opcion = true;
+                else
+                    System.out.println("Ingrese una opción válida\n");
+            } catch (Exception e) {
+                System.out.println("Ingrese una opción válida\n");
+            }
+        }
+
+        if(version == 1) {
+            vectorHeap(pacientes);
+        } else {
+            javaCollectionFramework(pacientes);
+        }
+    }
+
+    private void javaCollectionFramework(ArrayList<Paciente> pacientes) {
+        PriorityQueue<Paciente> pacientes_ordenados = new PriorityQueue<>();
         pacientes.forEach(paciente -> {
             pacientes_ordenados.add(paciente);
         });
-
         boolean validacion_pacientes = !pacientes_ordenados.isEmpty();
         Scanner sc = new Scanner(System.in);
         System.out.println("Bienvenido doctor, estos son los pacientes a atender hoy: ");
-        
-        while(validacion_pacientes) {
+
+        while (validacion_pacientes) {
             boolean validacion_opcion = false;
             Paciente paciente_actual = pacientes_ordenados.remove();
-            System.out.println("El paciente a atender es: " + paciente_actual.getNombre() + " con el diagnostico de: '" + paciente_actual.getEnfermedad() + "' con prioridad: '" + paciente_actual.getPrioridad() + "'");
+            System.out.println("El paciente a atender es: " + paciente_actual.getNombre() + " con el diagnostico de: '"
+                    + paciente_actual.getEnfermedad() + "' con prioridad: '" + paciente_actual.getPrioridad() + "'");
             String option = "";
             while (!validacion_opcion) {
                 System.out.println("¿Mostrar siguiente paciente?: Y/N");
@@ -35,17 +64,56 @@ public class Vista {
                     System.out.println("Ingrese una opción válida\n");
                 }
             }
-            if(option.equals("N") || option.equals("n")) {
+            if (option.equals("N") || option.equals("n")) {
                 System.out.println("Gracias por usar el sistema.");
                 break;
             }
             validacion_pacientes = !pacientes_ordenados.isEmpty();
         }
 
-        if(!validacion_pacientes) {
+        if (!validacion_pacientes) {
             System.out.println("Ya no existen pacientes por atender hoy, buen día.");
         }
+    }
 
+    private void vectorHeap(ArrayList<Paciente> pacientes) {
+        VectorHeap<Paciente> pacientes_ordenados = new VectorHeap<>();
+        pacientes.forEach(paciente -> {
+            pacientes_ordenados.add(paciente);
+        });
+
+        boolean validacion_pacientes = !pacientes_ordenados.isEmpty();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Bienvenido doctor, estos son los pacientes a atender hoy: ");
+
+        while (validacion_pacientes) {
+            boolean validacion_opcion = false;
+            Paciente paciente_actual = pacientes_ordenados.remove();
+            System.out.println("El paciente a atender es: " + paciente_actual.getNombre() + " con el diagnostico de: '"
+                    + paciente_actual.getEnfermedad() + "' con prioridad: '" + paciente_actual.getPrioridad() + "'");
+            String option = "";
+            while (!validacion_opcion) {
+                System.out.println("¿Mostrar siguiente paciente?: Y/N");
+                try {
+                    option = sc.next();
+                    if (option.equals("Y") || option.equals("N") || option.equals("y") || option.equals("n"))
+                        validacion_opcion = true;
+                    else
+                        System.out.println("Ingrese una opción válida\n");
+                } catch (Exception e) {
+                    System.out.println("Ingrese una opción válida\n");
+                }
+            }
+            if (option.equals("N") || option.equals("n")) {
+                System.out.println("Gracias por usar el sistema.");
+                break;
+            }
+            validacion_pacientes = !pacientes_ordenados.isEmpty();
+        }
+
+        if (!validacion_pacientes) {
+            System.out.println("Ya no existen pacientes por atender hoy, buen día.");
+        }
     }
 
 }
